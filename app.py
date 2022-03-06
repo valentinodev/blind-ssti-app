16e7dc31b1cab03d776c5c9c893cbe2c32bc39d2
valentinodev <70417739+valentinodev@users.noreply.github.com>
2022-03-06 11:29:17 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, url_for, request, redirect, render_template_string
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<p>%r</p> <a href="/admin/delete/%s">Delete</a>' % (self.content, self.id)
+
+@app.route('/', methods=['GET'])
+def index():
+    feedback = request.args.get("feedback")
+    if feedback:
+        feedback = Todo(content=feedback)
+        db.session.add(feedback)
+        db.session.commit()
+        return 'Thanks for sending your feedback'
+    else:
+        return 'Please, send your feedback'
+
+@app.route('/admin/', methods=['GET'])
+def admin():
+    password = request.args.get("password")
+    if password == 'admin':
+        feedbacks = Todo.query.all()
+        return render_template_string("<div>%s</div>" % feedbacks)
+    else:
+        return 'Wrong Password'
+
+@app.route('/admin/delete/<int:id>', methods=['GET'])
+def delete(id):
+    feedback = Todo.query.get_or_404(id)
+    db.session.delete(feedback)
+    db.session.commit()
+    return 'The feedback has been deleted'
+
+if __name__ == "__main__":
+    app.run(debug=True)
